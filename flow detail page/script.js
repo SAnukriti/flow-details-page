8d5715ae9409af18562c2104247ef5371b4158a1
anukritis1101 <anukritis1101@gmail.com>
2025-03-25 15:23:40 +0530

    Arrange the files and add response to source field

---
+++ flow detail page/script.js
@@ -0,0 +1,52 @@
+function insertMarkup(selector, value) {
+    if (value) {
+        document.querySelector(selector).insertAdjacentHTML('beforeend', `${value}`);
+    } else {
+        console.log(`Data missing for ${selector}`);
+    }
+}
+function insertFilter(selector, value){
+    if (value){
+        console.log("Value: ", value)
+        for (let i = 0; i < value.length; i++){
+            document.querySelector(selector).insertAdjacentHTML('beforeend', `${value[i].sourceField.label} <span class="label">${value[i].operator.label}</span> ${value[i].valueToBeCompared}`);
+        }
+    } else {
+        console.log(`Data missing for ${selector}`);
+    }
+}
+function insertUniqueField(selector, value){
+    if (value){
+        console.log("Value: ", value)
+        for (let i = 0; i < value.length; i++){
+            document.querySelector(selector).insertAdjacentHTML('beforeend', `${value[i].label} ${value[i].value} ${value[i].required}`);
+        }
+    } else {
+        console.log(`Data missing for ${selector}`);
+    }
+}
+function insertResponseToSource(selector, value){
+    if (value){
+        console.log("Value: ", value)
+        for (let i = 0; i < value.length; i++){
+            document.querySelector(selector).insertAdjacentHTML('beforeend', `${value[i].value} <span class="mapping"> > </span> ${value[i].from} `);
+        }
+    } else {
+        console.log(`Data missing for ${selector}`);
+    }
+}
+fetch('http://localhost:4000/api/v1/flow/get-flowdetails/01J7X1CXCV2QZV440TRDF57EN5')
+    .then(res => res.json())
+    .then(data => {
+        insertMarkup('#flowType', data.type);
+        insertMarkup('#firstExecution', data.startDate);
+        insertMarkup('#nextExecution', data.endDate);
+        insertMarkup('#createAt', data.created_time_field);
+        insertMarkup('#modifiedAt', data.updated_time_field);
+        insertFilter('#filters', data.filters);
+        insertUniqueField('#uniqueField', data.unique_fields);
+        insertResponseToSource('#responseToSource', data.response_field_mapping);
+        insertResponseToSource('#responseToDestination', data.response_field_mapping);
+        
+    })
+    .catch(error => console.log("Fetch Error:", error));
